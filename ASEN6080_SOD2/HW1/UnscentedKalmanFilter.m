85e0b179595641353636e1603accbab89abd5351
John Clouse <clous0dd@gmail.com>
2016-02-23 22:23:31 -0700

    More UKF fixes

--- ASEN6080_SOD2/HW1/UnscentedKalmanFilter.m
+++ ASEN6080_SOD2/HW1/UnscentedKalmanFilter.m
@@ -2,6 +2,7 @@ function output = UnscentedKalmanFilter(state_ap, P_ap, meas_store, fo)
 
 ObsData = meas_store;
 [num_obs, ~] = size(ObsData);
+% num_obs = 500;
 obs_r_idx = 3;
 obs_rr_idx = 4;
 obs_t_idx = 2;
@@ -9,7 +10,7 @@ obs_site_idx = 1;
 
 site = fo.site;
 theta_dot = fo.theta_dot;
-fo.ode_opts = odeset('RelTol', 1e-12, 'AbsTol', 1e-20);
+fo.ode_opts = odeset('RelTol', 1e-10, 'AbsTol', 1e-10);
 
 L = fo.UKF.L; 
 alpha = fo.UKF.alpha; 
@@ -23,13 +24,15 @@ w_i = 1/(2*(L+lambda));
 
 num_sig_pts = (2*L+1);
 
+% weights in matrix to get rid of looping.
+mean_weights = repmat([w_0_m repmat(w_i,1,num_sig_pts-1)],L,1);
+
 P = P_ap;
 X_est = state_ap;
 % R;
 % FIXME
 sig_range = 0.01; % m
 sig_rangerate = 0.001; %m/s
-% W = [1/(sig_range*sig_range) 0; 0 1/(sig_rangerate*sig_rangerate)];
 R = [(sig_range*sig_range) 0; 0 (sig_rangerate*sig_rangerate)];
 last_obs_time = 0;
 
@@ -74,6 +77,8 @@ for ii = 1:num_obs
         sig_pts_new = reshape(X(end,:)',L,num_sig_pts);
     else
         sig_pts_new = sig_pts;
+%         X_ap = X_est;
+%         Pbar_t = P;
     end
     
     % Time update
@@ -81,62 +86,60 @@ for ii = 1:num_obs
     for jj = 2:num_sig_pts
         X_ap = X_ap + w_i*sig_pts_new(:,jj);
     end
-    
+    X_ap = sum(mean_weights.*sig_pts_new,2);
+
     Pbar_t = w_0_c*(sig_pts_new(:,1) - X_ap)*(sig_pts_new(:,1) - X_ap)';
     for jj = 2:num_sig_pts
         Pbar_t = Pbar_t + ...
             w_i*(sig_pts_new(:,jj) - X_ap)*(sig_pts_new(:,jj) - X_ap)';
     end
-    
+
     if fo.use_SNC ...
             && dt < fo.SNC_meas_separation_threshold
-        
+
         % Compute Q. Use the state from last obs for any transformations.
         Q = compute_SNC_Q(fo, X_est(1:L));
         Gamma = fo.SNC_Gamma(dt);
 
         Pbar_t = Pbar_t + Gamma*Q*Gamma'; % Add the process noise
-%     elseif fo.use_DMC
-%         Q = compute_DMC_Q(fo,dt);
-%         P_ap = P_ap + Q; % DMC process noise
     end
-    
+
     % Recompute sig-pts
+    % These are basically a priori sig pts
     sqrt_Pbar = sqrtm(Pbar_t);
     sig_pts = [X_ap, repmat(X_ap,1,L) + gamma*sqrt_Pbar, ...
         repmat(X_ap,1,L) - gamma*sqrt_Pbar];
     
     % Computed measurements and weighted averages
     y_mean = zeros(2,1);
-    y_meas = zeros(2,2*L+1);
+    y_sig_comp = zeros(2,2*L+1);
     for jj = 1:num_sig_pts
         r_comp = compute_range_ECFsite(sig_pts(1:3,jj),...
             site(site_num).r*1e3,theta_dot*t_obs);
         rr_comp = compute_range_rate_ECFsite(sig_pts(1:6,jj),...
             site(site_num).r*1e3,theta_dot*t_obs, theta_dot);
-        y_meas(:,jj) = [r_comp; rr_comp];
+        y_sig_comp(:,jj) = [r_comp; rr_comp];
         if jj == 1
-            y_mean = y_mean + w_0_m*y_meas(:,jj);
+            y_mean = y_mean + w_0_m*y_sig_comp(:,jj);
         else
-            y_mean = y_mean + w_i*y_meas(:,jj);
+            y_mean = y_mean + w_i*y_sig_comp(:,jj);
         end
     end
     
     % Innovation and cross-correlation
-    Pyy = R + w_0_c*(y_meas(:,1)-y_mean)*(y_meas(:,1)-y_mean)';
-    Pxy = w_0_c*(sig_pts_new(:,1) - X_ap)*(y_meas(:,1)-y_mean)';
+    Pyy = R + w_0_c*(y_sig_comp(:,1)-y_mean)*(y_sig_comp(:,1)-y_mean)';
+    Pxy = w_0_c*(sig_pts_new(:,1) - X_ap)*(y_sig_comp(:,1)-y_mean)';
     for jj = 2:num_sig_pts
-        Pyy = Pyy + w_i*(y_meas(:,jj)-y_mean)*(y_meas(:,jj)-y_mean)';
+        Pyy = Pyy + w_i*(y_sig_comp(:,jj)-y_mean)*(y_sig_comp(:,jj)-y_mean)';
         Pxy = Pxy + ...
-            w_i*(sig_pts_new(:,jj) - X_ap)*(y_meas(:,jj)-y_mean)';
+            w_i*(sig_pts_new(:,jj) - X_ap)*(y_sig_comp(:,jj)-y_mean)';
     end
     
     % Measurement update
-%     chol_Pyy = chol(Pyy,'lower');
-%     Pyy_inv = eye(2)/(chol_Pyy')/(chol_Pyy);
     Kt = Pxy/Pyy;
     X_est = X_ap + Kt*(y_obs - y_mean);
-    P = Pbar_t - Kt*Pyy*Kt';
+%     P = Pbar_t - Kt*Pyy*Kt';
+    P = Pbar_t - Pxy*Kt'; % this removes an inversion
     
     % Post-fit residuals
     if alpha == 1
@@ -149,11 +152,11 @@ for ii = 1:num_obs
             site(site_num).r*1e3,theta_dot*t_obs);
         rr_comp = compute_range_rate_ECFsite(sig_pts(1:6,jj),...
             site(site_num).r*1e3,theta_dot*t_obs, theta_dot);
-        y_meas = [r_comp; rr_comp];
+        y_sig_comp = [r_comp; rr_comp];
         if jj == 1
-            y_mean = y_mean + w_0_m*y_meas;
+            y_mean = y_mean + w_0_m*y_sig_comp;
         else
-            y_mean = y_mean + w_i*y_meas;
+            y_mean = y_mean + w_i*y_sig_comp;
         end
     end
     end
