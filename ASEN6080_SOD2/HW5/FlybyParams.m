570a4ac3373d5985ab73f7945623ffb9cc488978
John Clouse <clous0dd@gmail.com>
2016-03-05 17:14:17 -0700

    Trying to get the error ellipse to work

--- ASEN6080_SOD2/HW5/FlybyParams.m
+++ ASEN6080_SOD2/HW5/FlybyParams.m
@@ -190,6 +190,29 @@ ode_opts = odeset('RelTol', 1e-12, 'AbsTol', 1e-20);
 % aim point: r_inf projected on the B plane
 BT = dot(X_to_SOI(end,1:3)',B_plane(:,2));
 BR = dot(X_to_SOI(end,1:3)',B_plane(:,3));
+
+STM_DCO_intercept = reshape(X(end,8:end)',7,7);
+P_intercept = STM_DCO_intercept*output_3.final_P*STM_DCO_intercept';
+P_int_Bplane = B_plane*P_intercept(1:3,1:3)*B_plane';
+[U,D] = eig(P_int_Bplane(2:3,2:3));
+ellip_tilt = atan2(U(2,1),-U(1,1));
+
+a=3*sqrt(max(max(D))); % horizontal radius
+b=3*sqrt(min(max(D))); % vertical radius
+x0=BT; % x0,y0 ellipse centre coordinates
+y0=BR;
+t=-pi:0.01:pi;
+x=a*cos(t);
+y=b*sin(t);
+ell_rot = [cos(ellip_tilt) -sin(ellip_tilt); sin(ellip_tilt) cos(ellip_tilt)];
+coords_prime = [];
+for ii = 1:length(t)
+    coords_prime(:,ii) = ell_rot*[x(ii); y(ii)];
+end
+figure
+plot(x0+coords_prime(1,:),y0+coords_prime(2,:))
+axis equal
+
 %% plots
 for ii = 1:7
 figure
