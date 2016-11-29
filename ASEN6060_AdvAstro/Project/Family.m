8c1c187986404396013086db920f6657019b925d
John Clouse <jdclouse@users.noreply.github.com>
2016-11-29 15:04:36 -0700

    Family generator working
    work done over thxgiving

--- ASEN6060_AdvAstro/Project/Family.m
+++ ASEN6060_AdvAstro/Project/Family.m
@@ -4,8 +4,10 @@ close all
 clear all
 
 % Initialize some things.
-addpath('C:\Users\John\Documents\Astro\ASEN5050\tools');
-addpath('C:\Users\John\Documents\Astro\ASEN5010\tools');
+% root = 'C:\Users\John\Documents\Astro';
+root = 'C:\Users\jclouse1\Documents\Class\AstroTools-master';
+addpath([root '\ASEN5050\tools']);
+addpath([root '\ASEN5010\tools']);
 CelestialConstants;
 
 hw_pub.figWidth = 1120; % pixels
@@ -45,6 +47,14 @@ r_init = Moon.R+alt_init;
 v_init = sqrt(Moon.mu/r_init);
 T_init = 2*pi*sqrt(r_init^3/Moon.mu);
 
+% And a slightly smaller one to compute a family tangent to feed into the
+% SSDR.
+fam_tan_alt_diff = 50;
+alt_init2 = alt_init - fam_tan_alt_diff; %km
+r_init2 = Moon.R+alt_init2;
+v_init2 = sqrt(Moon.mu/r_init2);
+T_init2 = 2*pi*sqrt(r_init2^3/Moon.mu);
+
 % Convert to the synodic frame, starting on the line between Earth and
 % Moon.
 first_guess_state = [moon_dist-r_init;0;0;0;v_init;0];
@@ -70,7 +80,7 @@ while abs(d) > tol
 
     [T_out,X_out] = ...
         ode45(@SSDR_deriv, [0,P/2], X, ...
-        odeset('Events', @y_crossing),propatagor_opts);
+        odeset('Events', @y_crossing, 'RelTol', 3e-14, 'AbsTol', 1e-20),propatagor_opts);
 
 %     d = -[X_out(end,4); X_out(end,6)];
     d = -X_out(end,4);
@@ -87,6 +97,8 @@ while abs(d) > tol
         - 1/y_dot*state_dot(4)* STM(2,5));
 %     correction = pinv([STM(1,5); STM(4,5)] ...
 %         - 1/y_dot*[state_dot(1);state_dot(4)]*STM(2,5))*d;
+%     correction = ([STM(1,1) STM(1,5); STM(4,1) STM(4,5)] ...
+%         - 1/y_dot*[state_dot(1);state_dot(4)]*[STM(2,1) STM(2,5)])\d;
 
 %     X_ini(1) = X_ini(1) + correction(1);
 %     X_ini(5) = X_ini(5) + correction(2);
@@ -101,15 +113,70 @@ plot(X(:,1),X(:,2))
 hold on
 axis equal
 
+first_PO_state = X_ini;
+first_PO_T = T_out(end);
 
-%%
+% A second orbit to start a family tangent calculation.
 
-[X_out, T_out] = SSDR(first_guess_state, T_init, propatagor_opts);
+d = [1;1];
+tol = 1e-13;
+first_guess_state2 = [moon_dist-r_init2;0;0;0;v_init2;0];
+X_ini = first_guess_state2;
+while abs(d) > tol 
+    X = [X_ini; reshape(eye(6),36,1)];
+
+    [T_out,X_out] = ...
+        ode45(@SSDR_deriv, [0,P/2], X, ...
+        odeset('Events', @y_crossing, 'RelTol', 3e-14, 'AbsTol', 1e-20),propatagor_opts);
+
+    d = -X_out(end,4);
+    % STM
+    STM = reshape(X_out(end,7:end),6,6);
+    y_dot = X_out(end,5);
+    state_dot = Lagrange_CR3BP(0,X_out(end,1:6)',propatagor_opts);
+
+    % The correction while holding x constant
+    correction = d/( STM(4,5) ...
+        - 1/y_dot*state_dot(4)* STM(2,5));
+    
+    X_ini(5) = X_ini(5) + correction;
+    d
+end
 
 [Times,X] = ...
-    ode45(@Lagrange_CR3BP,[0 10*T_out], X_out, ...
+    ode45(@Lagrange_CR3BP,[0 10*T_out(end)], X_ini, ...
     ode_opts, propatagor_opts);
-figure
-plot(X(:,1),X(:,2))
-hold on
-axis equal
+plot(X(:,1),X(:,2),'r')
+
+zeroth_PO_state = X_ini;
+zeroth_PO_T = T_out(end);
+
+fam_tan_state = first_PO_state-zeroth_PO_state;
+fam_tan_T = first_PO_T-zeroth_PO_T;
+
+fam_tan_norm = norm([norm(fam_tan_state) norm(fam_tan_T)]);
+fam_tan_norm = norm([fam_tan_state; fam_tan_T]);
+fam_tan_state = fam_tan_state/fam_tan_norm;
+fam_tan_T = fam_tan_T/fam_tan_norm;
+
+%%
+state_in = first_PO_state;
+time_in = first_PO_T;
+family_tangent = [fam_tan_state;fam_tan_T];
+%%
+for ii = 1:1000
+    ii
+    [X_out, T_out] = SSDR(state_in, time_in, family_tangent, 1000, propatagor_opts);
+    if mod(ii,50) == 0
+        [Times,X] = ...
+            ode45(@Lagrange_CR3BP,[0 T_out], X_out, ...
+            ode_opts, propatagor_opts);
+        % figure
+        plot(X(:,1),X(:,2),'k')
+    end
+    % hold on
+    % axis equal
+    family_tangent = [X_out-state_in;T_out-time_in]/norm([X_out-state_in;T_out-time_in]);
+    state_in = X_out;
+    time_in = T_out;
+end
--- ASEN6060_AdvAstro/Project/SSDR.m
+++ ASEN6060_AdvAstro/Project/SSDR.m
@@ -1,28 +1,48 @@
 %% SSDR - single shooting differential corrector.
-function [X_out, T_out] = SSDR( X_init, T_init, prop_opts )
+function [X_out, T_out] = SSDR( X_init, T_init, fam_tan, delta_s, prop_opts )
 
-ode_opts = odeset('RelTol', 1e-8, 'AbsTol', 1e-9);
-Xi = X_init;
-Ti = T_init;
-dx_store = [];
-F_store = [];
+ode_opts = odeset('RelTol', 1e-12, 'AbsTol', 1e-12);
+% predict the next orbit w/ pseudo arc len continuation
+% delta_s = 1000;
+Xi = X_init + delta_s*fam_tan(1:end-1);
+Ti = T_init + delta_s*fam_tan(end);
+% dx_store = [];
+% F_store = [];
+% dx_norm_store = [];
+% we are starting from a previously-defined orbit
+x_tilde_dot = Lagrange_CR3BP(0,X_init, prop_opts);
+
+dx = ones(7,1);
+
 for ii = 1:20
+    if norm(dx) < 1e-9
+        break;
+    end
 [T,X] = ode45(@SSDR_deriv,[0 Ti], [Xi; reshape(eye(6),36,1)], ...
     ode_opts, prop_opts);
 params_state_dot = Lagrange_CR3BP(0,X(end,1:6)', prop_opts);
-F = [Xi - X(end,1:6)'; dot(Xi(4:6),[1;0;1])];
+% Xi(1)-X_init(1)
+curr_delta_s = dot([Xi-X_init;Ti-T_init],fam_tan) ;
+% F = [Xi - X(end,1:6)'; dot(Xi-X_init,x_tilde_dot); -delta_s+curr_delta_s];
+F = -[X(end,1:6)' - Xi; dot(Xi-X_init,x_tilde_dot); +curr_delta_s-delta_s];
 
-A = [reshape(X(end,7:end),6,6), params_state_dot;...
-     0, 0, 0, 1, 0, 1, 0];
+A = [reshape(X(end,7:end),6,6)-eye(6), params_state_dot;...
+     x_tilde_dot', 0; fam_tan'];
 
-dx = A\F;
+% dx = A\F;
+dx = pinv(A)*F;
 Xi = Xi + dx(1:6);
 Ti = Ti + dx(7);
-dx_store = [dx_store, dx];
-F_store = [F_store, F];
+% dx_store = [dx_store, dx];
+% F_store = [F_store, F];
+% dx_norm_store = [dx_norm_store norm(dx)];
 end
-dx_store
-F_store
+if ii == 20
+    fprintf('Hit 20 loops');
+end
+% dx_store
+% F_store
+% dx_norm_store
 % reshape(X(end,7:end),6,6)
 T_out = T(end);
 X_out = X(end,1:6)';
